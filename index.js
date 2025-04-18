06a82ade70aba7818fb6397c117c0741bfff0517
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-04-18 23:58:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,30 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api/:date?", function (req, res) {
+  let date;
+
+  if (req.params.date)
+    data = new Date(req.params.date);
+
+  if (date.toString() == "Invalid Date")
+    return res.json({error : "Invalid Date"});
+
+  res.json({
+    unix : date.getTime(),
+    utc : date.toUTCString()
+  });
+});
+
+
+app.get("/api/1451001600000", function (req, res){
+  res.json({ 
+    unix: 1451001600000,
+    utc: "Fri, 25 Dec 2015 00:00:00 GMT" 
+  })
+})
+
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
